96091b7ef1221deceba396e4a9f25b66c2921572
TzeMingHo <tzemingho@gmail.com>
2026-07-23 10:20:36 +0100

    frontend-setup

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import "../index.css";
+
+function App() {
+  return (
+    <h1 className="font-display text-3xl font-bold text-accent overshoot rise rise-2">
+      Hello Team Penguins! 🐧
+    </h1>
+  );
+}
+
+export default App;
